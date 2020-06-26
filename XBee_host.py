ad2cce00673478ec52adf76fe64562bcd005d14b
JasonYang <jason53240@gmail.com>
2020-06-26 11:18:39 +0800

    1

---
+++ XBee_host.py
@@ -0,0 +1,88 @@
+import serial
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+import paho.mqtt.client as paho
+
+mqttc = paho.Client()
+host = "localhost"
+topic = "Mbed"
+port = 1883
+
+# Callbacks
+def on_connect(self, mosq, obj, rc):
+    print("Connected rc: " + str(rc))
+
+def on_message(mosq, obj, msg):
+    print("[Received] Topic: " + msg.topic + ", Message: " + str(msg.payload) + "\n");
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+    print("Subscribed OK")
+
+def on_unsubscribe(mosq, obj, mid, granted_qos):
+    print("Unsubscribed OK")
+
+# Set callbacks
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_subscribe = on_subscribe
+mqttc.on_unsubscribe = on_unsubscribe
+
+# Connect and subscribe
+print("Connecting to " + host + "/" + topic)
+mqttc.connect(host, port=1883, keepalive=60)
+mqttc.subscribe(topic, 0)
+
+# XBee setting
+serdev = '/dev/ttyUSB0'
+s = serial.Serial(serdev, 9600)
+
+s.write("+++".encode())
+char = s.read(2)
+print("Enter AT mode.")
+print(char.decode())
+
+s.write("ATMY 0x148\r\n".encode())
+char = s.read(3)
+print("Set MY 0x148.")
+print(char.decode())
+
+s.write("ATDL 0x248\r\n".encode())
+char = s.read(3)
+print("Set DL 0x248.")
+print(char.decode())
+
+s.write("ATID 0x1\r\n".encode())
+char = s.read(3)
+print("Set PAN ID 0x1.")
+print(char.decode())
+
+s.write("ATWR\r\n".encode())
+char = s.read(3)
+print("Write config.")
+print(char.decode())
+
+s.write("ATMY\r\n".encode())
+char = s.read(4)
+print("MY :")
+print(char.decode())
+
+s.write("ATDL\r\n".encode())
+char = s.read(4)
+print("DL : ")
+print(char.decode())
+
+s.write("ATCN\r\n".encode())
+char = s.read(3)
+print("Exit AT mode.")
+print(char.decode())
+
+finish = 0
+while (finish):
+    message = s.readline().decode()
+    mqttc.publish(topic, message)
+    print(message)
+    time.sleep(1)
+
+s.close()
+
